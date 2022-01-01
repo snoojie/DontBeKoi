a07bf215cbf74b9f31144b0d0b262fa1d9caa908
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2022-01-01 14:03:20 -0500

    code cleanup and comments on list command

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,6 +1,6 @@
 import { Collection, CommandInteraction, GuildBasedChannel, Message, MessageAttachment, MessageEmbed, MessageManager, MessageReaction, ReactionUserManager, TextChannel, User } from "discord.js";
 import { KoiCommand } from "../structures/command/koiCommand";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import cheerio, { CheerioAPI } from "cheerio";
 import * as Canvas from "canvas";
 
@@ -8,71 +8,74 @@ class ListCommand extends KoiCommand
 {
     constructor()
 	{
-        super("list", "List players who need a specific koi.", true);
+        super(
+            "list", 
+            "List everyone who need a specific koi. Ex usage: /list shigin usagi", 
+            true
+        );
 	}
 
 	public async execute(interaction: CommandInteraction): Promise<void>
     {
         await interaction.deferReply();
 
-        if (!interaction.guild)
-        {
-            await this.replyWithError(
-                interaction, "This interaction is not associated with a guild."
-            );
-            return;
-        }
-
         // pattern as provided by the discord user
         const PATTERN = this.getOptionValuePattern(interaction);
         
         // color name as provided by the discord user
-        const COLOR = this.getColor(interaction);
+        const COLOR = this.getOptionValueColor(interaction);
 
         // check that the channel of this pattern exists
-        let channel: TextChannel | undefined = 
-            this.getChannel(interaction, PATTERN);
+        let channel: TextChannel | undefined = this.getChannel(interaction, PATTERN);
         if (!channel)
         {
-            console.error(`User requested to list everyone who needs ${PATTERN} but its channel does not exist`);
-            interaction.editReply(`There isn't a channel yet for ${PATTERN}.`);
+            await this.replyWithError(
+                interaction, `There isn't a channel yet for ${PATTERN}`, true
+            );
             return;
         }
 
         // find our color in the pattern channel
         // note we cannot use the messages cache as it is empty
-        const MESSAGES: Collection<string, Message<boolean>> = await channel.messages.fetch();
-        let colorIndex = 32;
+
+        const MESSAGES: Collection<string, Message<boolean>> = 
+            (await channel.messages.fetch())
+            .reverse()
+            .filter(message => message.attachments.size>0);
+        
+        // this index is used to determine rarity
+        let index = -1;
+
         for (let [messageId, message] of MESSAGES)
         {
-            const COLOR_IMAGE: MessageAttachment | undefined = message.attachments.first();
-            if (!COLOR_IMAGE)
-            {
-                // this is likely the message starting commons or rares
-                // or a line break
-                continue;
-            }
-            colorIndex--;
+            index++;
 
-            if (!COLOR_IMAGE.name || !COLOR_IMAGE.name.toLowerCase().startsWith(COLOR))
-            {
+            const DRAWING: MessageAttachment | undefined = message.attachments.first();
 
+            if (!DRAWING || 
+                !DRAWING!.name || 
+                !DRAWING!.name.toLowerCase().startsWith(COLOR)
+            )
+            {
                 // this isn't the right color
                 continue;
             }
 
             // found our color!
-            const NEED: MessageReaction | undefined = 
+            const NEED_REACTION: MessageReaction | undefined = 
                 message.reactions.cache.get(this.REACTION_NEED);
-            if (!NEED)
+            if (!NEED_REACTION)
             {
-                await this.replyWithError(interaction, `No need reaction found on ${COLOR} ${PATTERN}.`);
+                // this shouldn't happen
+                await this.replyWithError(
+                    interaction, `No need reaction found on ${COLOR} ${PATTERN}.`, true
+                );
                 return;
             }
 
             // get everyone who needs this colored pattern
             let people: string[] = []
-            for (let [userId, user] of NEED.users.cache)
+            for (let [userId, user] of NEED_REACTION.users.cache)
             {
                 if (!user.bot)
                 {
@@ -80,7 +83,7 @@ class ListCommand extends KoiCommand
                 }
             }
 
-            const RARITY = colorIndex<16 ? "common" : "rare";
+            const RARITY = index<16 ? "common" : "rare";
 
             // the text to display back to the user
             let replyText: string = `${RARITY} ${COLOR} ${PATTERN}`;
@@ -98,7 +101,7 @@ class ListCommand extends KoiCommand
             const IMAGE_URL: string = await axios
                 .create()
                 .get(`https://zenkoi2.fandom.com/wiki/${PATTERN}`)
-                .then(function(response) {
+                .then(function(response: AxiosResponse<any, any>): string {
                     
                     // response.data is the html
                     const $: CheerioAPI = cheerio.load(response.data);
@@ -107,48 +110,53 @@ class ListCommand extends KoiCommand
                     const IMAGE_KEY = 
                         PATTERN[0].toUpperCase() + PATTERN.slice(1) + "_" + // ex: usagi -> Usagi
                         RARITY + "_" + 
-                        (1+(Math.floor(colorIndex/4)%4));                   // file number
+                        (1+(Math.floor(index/4)%4)); // file number between [1,4]
                     
                     // return the url of this image
                     // note the url data attribute gives something like
                     // https://static.wikia.nocookie.net/zenkoi2/images/1/16/Usagi_common_1.jpg/revision/latest/scale-to-width-down/177?cb=20180514192047
                     // strip everything after .jpg
-                    const URL: string = <string>$(`[data-image-key^="${IMAGE_KEY}"]`).data("src");
+                    const URL: string = 
+                        <string>$(`[data-image-key^="${IMAGE_KEY}"]`).data("src");
                     if (!URL)
                     {
-                        console.error(`Couldn't find the image URL for ${COLOR} ${PATTERN}`);
+                        console.error(
+                            `Couldn't find the image URL for ${COLOR} ${PATTERN}`
+                        );
                         return "";
                     }
                     const KEY_INDEX = URL.indexOf(IMAGE_KEY);
                     if (!KEY_INDEX)
                     {
-                        console.error(`Can't remove .jpg or .png from image url for ${COLOR} ${PATTERN}: ${URL}.`);
+                        console.error(
+                            `Can't remove .jpg or .png from image url for ${COLOR} ${PATTERN}: ${URL}.`
+                        );
                         return "";
                     }
                     return URL.substring(0, KEY_INDEX+IMAGE_KEY.length+4);
                 })
-                .catch(function(error) {
+                .catch(function(error): string {
                     console.error(`Failed to access wiki for ${COLOR} ${PATTERN}.`);
                     return "";
                 });
 
-            // by default, show the color image
+            // by default, show the drawing from the pattern channel
             // this will be displayed if the wiki is missing an image for this fish
-            let replyDrawing: MessageAttachment = COLOR_IMAGE;
+            let replyDrawing: MessageAttachment = DRAWING;
             if (IMAGE_URL)
             {
-                const IMAGE = await Canvas.loadImage(IMAGE_URL);
-                const KOI_WIDTH = IMAGE.width / 2.62;   // tested with shikoji usagi, shidai fuwa
-                const KOI_HEIGHT = IMAGE.height / 4;
-                const CANVAS_WIDTH = KOI_WIDTH/2;
-                const CANVAS_HEIGHT = KOI_HEIGHT/2;
+                const IMAGE: Canvas.Image = await Canvas.loadImage(IMAGE_URL);
+                const KOI_WIDTH: number = IMAGE.width / 2.5;   // tested with shikoji and shiusu usagi
+                const KOI_HEIGHT: number = IMAGE.height / 4;
+                const CANVAS_WIDTH: number = KOI_WIDTH/2;
+                const CANVAS_HEIGHT: number = KOI_HEIGHT/2;
 
                 let canvas: Canvas.Canvas = new Canvas.Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
                 let context: Canvas.NodeCanvasRenderingContext2D = canvas.getContext("2d");
 
                 // this image has 4 colored koi
                 // only draw the one we need
-                const POSITION = colorIndex%4;
+                const POSITION = index%4;
                 context.drawImage(
                     IMAGE, 
                     0, POSITION*KOI_HEIGHT, KOI_WIDTH, KOI_HEIGHT, 
@@ -170,7 +178,9 @@ class ListCommand extends KoiCommand
         }
 
         // if this is reached, we could not find the color in the pattern channel
-	    await interaction.editReply(`Pattern ${PATTERN} does not have color ${COLOR}.`);
+        await this.replyWithError(
+            interaction, `Pattern ${PATTERN} does not have color ${COLOR}.`, true
+        );
 	}
 }
 
--- src/structures/command/koiCommand.ts
+++ src/structures/command/koiCommand.ts
@@ -1,6 +1,9 @@
 import { Command } from "./command";
 import { CommandInteraction, GuildBasedChannel, TextChannel } from "discord.js";
 
+const OPTION_PATTERN_NAME = "pattern";
+const OPTION_COLOR_NAME = "color";
+
 export abstract class KoiCommand extends Command
 {
     protected readonly REACTION_NEED = "🎣";
@@ -12,22 +15,38 @@ export abstract class KoiCommand extends Command
         
         if (colorOption)
         {
-            this.addOption(COLOR, "Color of the koi.");
+            this.addOption(OPTION_COLOR_NAME, "Koi color name, for example shigin.");
         }
-        this.addOption(PATTERN, "Name of the koi pattern.");
+        this.addOption(OPTION_PATTERN_NAME, "Koi pattern, for example usagi.");
     }
     
-    // confirmed both
-    protected getOptionValuePattern(interaction: CommandInteraction) : string
+    /**
+     * Gets the pattern the discord user entered for the pattern option.
+     * 
+     * /collector pattern:bukimi
+     * 
+     * Returns "bukimi"
+     * 
+     * @param interaction Interaction this slash command occured in.
+     * @returns Pattern as provided by the discord user.
+     */
+    protected getOptionValuePattern(interaction: CommandInteraction): string
     {
-        return this.getOptionValue(interaction, PATTERN);
+        return this.getOptionValue(interaction, OPTION_PATTERN_NAME);
     }
     
-    protected getColor(interaction: CommandInteraction) : string
+    /**
+     * Gets the color the discord user entered for the color option.
+     * 
+     * /list color:shigin pattern:usagi
+     * 
+     * Returns "shigin"
+     * 
+     * @param interaction Interaction this slash command occured in.
+     * @returns Color as provided by the discord user.
+     */
+    protected getOptionValueColor(interaction: CommandInteraction): string
     {
-        return this.getOptionValue(interaction, COLOR);
+        return this.getOptionValue(interaction, OPTION_COLOR_NAME);
     }
 }
-
-const PATTERN = "pattern";
-const COLOR = "color";
