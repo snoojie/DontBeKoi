cedda7243b4a96ac42eb295637974b90bc253e7f
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2022-01-17 14:08:19 -0500

    basic project agaiinnnn

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-import * as dotenv from "dotenv";
-
-// init environment variables
-dotenv.config();
-
-import { ExtendedClient } from "./structures/client/extendedClient";
-
-let client = new ExtendedClient();
-client.start();
+console.log("Hello");
