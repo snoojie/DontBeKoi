ac321286679300711dd71dd3fc92733d9711ee45
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2022-01-17 15:53:47 -0500

    Added logger

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("Hello");
+import { Logger } from "./util/logger";
+
+Logger.log("Hello!\nstill green?");
+Logger.error("errory message", new Error("blegh"));
+//console.trace();
---
+++ src/util/logger.ts
@@ -0,0 +1,39 @@
+// references: 
+// https://stackoverflow.com/a/41407246
+// https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html#colors
+enum Color
+{
+    GREEN    = "\x1b[32m%s\x1b[0m",
+    RED      = "\x1b[31m%s\x1b[0m",
+    RED_GREY = "\x1b[38;5;131m%s\x1b[0m"
+};
+
+enum Theme
+{
+    LOG        = Color.GREEN,
+    ERROR      = Color.RED,
+    STACKTRACE = Color.RED_GREY
+};
+
+export class Logger
+{
+    /**
+     * Replacement to console.log
+     * @param message message to print
+     */
+    public static log(message: string): void
+    {
+        console.log(Theme.LOG, message);
+    }
+
+    /**
+     * Replacement to console.error
+     * @param message Quick message to describe the error
+     * @param error Error itself
+     */
+    public static error(message: string, error: Error): void
+    {
+        console.log(Theme.ERROR, message);
+        console.log(Theme.STACKTRACE, error.stack);
+    }
+}
