4f4236f909f0ff37c7a04d2ce482fbf86a7bd03d
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2022-01-17 19:13:43 -0500

    sdif

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-console.log("Mee!p");
+import {Logger} from "./util/logger";
 
-throw new Error("error!!!fdgvsdfa!");
+Logger.log("I r log");
+Logger.error("I r error", new Error("omg i'm thrown"));
+//console.log("\x1b[32m%s\x1b[0m", "custom");
+//console.log("\u001b[38;5;2mHelloWorld\u001b[0m");
+//console.log("\u001b[38;5;2m%s\u001b[0m", "hi");
+console.log("post");
