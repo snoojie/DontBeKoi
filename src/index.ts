6cc0381ab3639d4c93d4ce9eb5fcf670c322f19c
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2022-01-18 14:58:45 -0500

    sdf

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import {Logger} from "./util/logger";
 
-Logger.log("I r log!!!!");
+Logger.log("I'm a happy log");
 Logger.error("I r error!!!!", new Error("omg i'm thrown"));
 console.log("post");
+
+/*console.log("\x1b[38;5;130m%s\x1b[0m", "zero");
+console.log("\x1b[38;5;131m%s\x1b[0m", "one");
+console.log("\x1b[38;5;132m%s\x1b[0m", "two");
+
+"\033[38;5;130mzero\033[0m"*/
