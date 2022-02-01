84039125dc6d9a5316780b86c51748a7b1822b8c
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2022-02-01 09:58:23 -0500

    improved tests for community spreadsheet get overview

--- tests/google/communitySpreadsheet.test.js
+++ tests/google/communitySpreadsheet.test.js
@@ -1,65 +1,67 @@
 const { CommunitySpreadsheet } =require("../../src/google/communitySpreadsheet");
 
+// ==================
+// =====OVERVIEW=====
+// ==================
+
 test("Overview includes collectors.", async () => {
     let overview = await CommunitySpreadsheet.getOverview();
-    let hasCollectors = false;
-    for (let entry of overview)
-    {
-        if (entry.type == "Collector")
-        {
-            hasCollectors = true;
-            break;
-        }
-    }
-    expect(hasCollectors).toBeTruthy();
+    expectOverviewHasType("Collector");
 });
 
 test("Overview includes progressives.", async () => {
     let overview = await CommunitySpreadsheet.getOverview();
-    let hasProgressives = false;
+    expectOverviewHasType(overview, "Progressive");
+});
+
+function expectOverviewHasType(overview, type)
+{
+    let hasType = false;
     for (let entry of overview)
     {
-        if (entry.type == "Progressive")
+        if (entry.type == type)
         {
-            hasProgressives = true;
+            hasType = true;
             break;
         }
     }
-    expect(hasProgressives).toBeTruthy();
-});
+    expect(hasType).toBeTruthy();
+}
 
 test("There are 30 progressives.", async () => {
     let overview = await CommunitySpreadsheet.getOverview();
-    let count = 0;
-    for (let entry of overview)
-    {
-        if (entry.type == "Progressive")
-        {
-            count++;
-        }
-    }
+    let count = getOverviewCountOfType(overview, "Progressive");
     expect(count).toBe(30);
 });
 
 test("There are at least 208 collectors.", async () => {
     let overview = await CommunitySpreadsheet.getOverview();
+    let count = getOverviewCountOfType(overview, "Collector");
+    expect(count).toBeLessThanOrEqual(208);
+});
+
+function getOverviewCountOfType(overview, type)
+{
     let count = 0;
     for (let entry of overview)
     {
-        if (entry.type == "Collector")
+        if (entry.type == type)
         {
             count++;
         }
     }
-    expect(count).toBeLessThanOrEqual(208);
-});
+    return count;
+}
 
-testSpecificPattern("Daimon", "Collector", 5);
-testSpecificPattern("Totemu", "Collector", 9);
-testSpecificPattern("Shapu", "Progressive", undefined);
-function testSpecificPattern(name, type, hatchTime)
+testOverviewHasPattern("Daimon", "Collector", 5);
+testOverviewHasPattern("Totemu", "Collector", 9);
+testOverviewHasPattern("Shapu", "Progressive", undefined);
+function testOverviewHasPattern(name, type, hatchTime)
 {
-    test(`${name} is a ${type.toLowerCase()} with a hatch time of ${hatchTime} hours.`, async () => {
+    test(
+        `${name} is a ${type.toLowerCase()} with a hatch time of ${hatchTime} hours.`, 
+        async () => 
+    {
         let overview = await CommunitySpreadsheet.getOverview();
         let found = false;
         for (let entry of overview)
