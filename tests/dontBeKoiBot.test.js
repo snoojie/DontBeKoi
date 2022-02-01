5104ef07e3e21883a190d99b672ade062431bc0b
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2022-01-31 19:47:55 -0500

    Added tests for bot

--- tests/dontBeKoiBot.test.js
+++ tests/dontBeKoiBot.test.js
@@ -5,7 +5,7 @@ const ErrorMessages = require("../src/errorMessages").default;
 // mute logger
 Logger.log = jest.fn();
 
-// the first time a bot is started, it takes about half a second
+// the first time the bot is started, it takes about half a second
 // but the following starts up are longer. 
 // The max recorded so far is just shy of a minute;
 // So, let's allow each startup to run for 1.5 minutes.
@@ -47,34 +47,59 @@ test("The bot can be started and stopped multiple times.", async () => {
 // =====LOGIN=====
 // ===============
 
-describe("Test login with invalid bot token.", () => {
+describe("Test with modified environment variables.", () => {
 
     beforeEach(() => {
         process.env = { ...ORIGINAL_ENV };
-        delete process.env.BOT_TOKEN
     });
     afterAll(() => process.env = ORIGINAL_ENV);
-    
-    test("Error starting a bot without a token.", async () =>  {
-        await expect(Bot.start()).rejects.toThrow(
-            ErrorMessages.CONFIG.MISSING_ENVIRONMENT_VARIABLE
-        );
+
+    describe("Test login with invalid bot token.", () => {
+
+        beforeEach(() => delete process.env.BOT_TOKEN);
         
+        test("Error starting the bot without a token.", async () =>  {
+            await expect(Bot.start()).rejects.toThrow(
+                ErrorMessages.CONFIG.MISSING_ENVIRONMENT_VARIABLE
+            );
+            
+        });
+
+        test("Error starting the bot with an incorrect token.", async () =>  {
+            process.env.BOT_TOKEN = "I am not valid";
+            await expect(Bot.start()).rejects.toThrow(ErrorMessages.BOT.FAILED_LOGIN);
+        });
+
+        testStartAfterFailedAttempt("login attempt");
+
     });
 
-    test("Error starting a bot with an incorrect token.", async () =>  {
-        process.env.BOT_TOKEN = "I am not valid";
-        await expect(Bot.start()).rejects.toThrow(ErrorMessages.BOT.FAILED_LOGIN);
+    // ==================
+    // =====COMMANDS=====
+    // ==================
+
+    describe("Test running the Command Manager with it in a failed state.", () => {
+
+        beforeEach(() => delete process.env.CLIENT_ID);
+        
+        test("Error starting the bot when cannot deploy commands.", async () =>  {
+            await expect(Bot.start()).rejects.toThrow();        
+        });
+
+        testStartAfterFailedAttempt("command deployment attempt");
+
     });
 
-    test("Can start the bot after a failed login attempt.", async () => {
+    function testStartAfterFailedAttempt(failedDescription)
+    {
+        test(`Can start the bot after a failed ${failedDescription}.`, async () => {
 
-        // purposely fail the start
-        await expect(Bot.start()).rejects.toThrow();
+            // purposely fail the start
+            await expect(Bot.start()).rejects.toThrow();
 
-        // run the bot successfully
-        process.env = ORIGINAL_ENV;
-        await Bot.start();
-    }, 2 * TIMEOUT);
-
+            // run the bot successfully
+            process.env = ORIGINAL_ENV;
+            await Bot.start();
+        }, 2 * TIMEOUT);
+    }
 });
