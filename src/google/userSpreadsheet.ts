3f231cac3ebd96c2ca697b0b0030553a7cbcc6c7
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2022-02-01 13:42:07 -0500

    haskoi now supports rare colors.

--- src/google/userSpreadsheet.ts
+++ src/google/userSpreadsheet.ts
@@ -1,5 +1,5 @@
 import ErrorMessages from "../errorMessages";
-import { PatternType } from "../types";
+import { PatternType, Rarity } from "../types";
 import KoiSpreadsheet from "./koiSpreadsheet";
 import Spreadsheet from "./spreadsheet";
 
@@ -74,7 +74,15 @@ const UserSpreadsheet = {
         }
 
         // find the highlight color
-        let highlightColorColumnIndex: number = -1;
+        let highlightColorColumnIndex: number = getHighlightColorColumnIndex(
+            TABLE, patternNameRowIndex, COLOR_LOWERCASE, Rarity.Common
+        );
+        if (highlightColorColumnIndex < 0)
+        {
+            highlightColorColumnIndex = getHighlightColorColumnIndex(
+                TABLE, patternNameRowIndex, COLOR_LOWERCASE, Rarity.Rare
+            );
+        }
         for (let i=0; i<4; i++)
         {
             const COLUMN_INDEX: number = i + 1;
@@ -103,9 +111,38 @@ const UserSpreadsheet = {
         const VALUE: string = KoiSpreadsheet.getStringFromCell(
             TABLE, baseColorRowIndex, highlightColorColumnIndex
         ).toLowerCase();
+        console.log("VALUE: " + VALUE +", (" + baseColorRowIndex + ", " + highlightColorColumnIndex + ")");
         return VALUE == "k" || VALUE == "d";
     } 
 
 };
 
 export default UserSpreadsheet;
+
+function getHighlightColorColumnIndex(
+    table: string[][], 
+    patternNameRowIndex: number, 
+    colorLowercase: string, 
+    rarity: Rarity
+): number
+{
+    let index: number = -1;
+
+    const RARITY_OFFSET: number = rarity == Rarity.Common ? 1 : 6;
+
+    for (let i=0; i<4; i++)
+    {
+        const COLUMN_INDEX: number = i + RARITY_OFFSET;
+        const HIGHLIGHT_COLOR: string = 
+            KoiSpreadsheet.getHighlightColorFromColumn(
+                table, patternNameRowIndex + 1, COLUMN_INDEX
+            );
+        if (colorLowercase.endsWith(HIGHLIGHT_COLOR.toLowerCase()))
+        {
+            // found the highlight color!
+            index = COLUMN_INDEX;
+            break;
+        }
+    }
+    return index;
+}
--- tests/google/userSpreadsheet.test.js
+++ tests/google/userSpreadsheet.test.js
@@ -4,87 +4,99 @@ const ErrorMessages = require("../../src/errorMessages").default;
 // ID of test user sheet
 const SPREADSHEET_ID = "1yt01AXsDvBrGpKyVETKlsgJhetUJq5eOMLx5Sf60TAU";
 
-// todo
-// these are prelimary tests. they have not yet been run
 
 test("User with common collector koi.", async() => {
-    const HAS_KOI = await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "ryomido", "akua");
+    const HAS_KOI = 
+        await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "ryomido", "akua", "Collector");
     expect(HAS_KOI).toBeTruthy();
 });
 
 test("User missing common collector koi.", async() => {
-    const HAS_KOI = await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "budai", "akua");
+    const HAS_KOI = 
+        await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "budai", "akua", "Collector");
     expect(HAS_KOI).not.toBeTruthy();
 });
 
 test("User with rare collector koi.", async() => {
-    const HAS_KOI = await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "akamura", "akua");
+    const HAS_KOI = 
+        await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "akamura", "akua", "Collector");
     expect(HAS_KOI).toBeTruthy();
 });
 
 test("User missing rare collector koi.", async() => {
-    const HAS_KOI = await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "akapapu", "akua");
+    const HAS_KOI = 
+        await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "akapapu", "akua", "Collector");
     expect(HAS_KOI).not.toBeTruthy();
 });
 
+// todo
+/*
 test("User with common progressive koi.", async() => {
-    const HAS_KOI = await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "akadai", "inazuma");
+    const HAS_KOI = 
+        await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "akadai", "inazuma", "Progressive");
     expect(HAS_KOI).toBeTruthy();
 });
 
 test("User missing common progressive koi.", async() => {
-    const HAS_KOI = await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "kishiro", "toraiu");
+    const HAS_KOI = 
+        await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "kishiro", "toraiu", "Progressive");
     expect(HAS_KOI).not.toBeTruthy();
 });
 
 test("User with rare progressive koi.", async() => {
-    const HAS_KOI = await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "akamido", "katame");
+    const HAS_KOI = 
+        await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "akamido", "katame", "Progressive");
     expect(HAS_KOI).toBeTruthy();
 });
 
 test("User missing rare progressive koi.", async() => {
-    const HAS_KOI = await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "kuburu", "katame");
+    const HAS_KOI = 
+        await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "kuburu", "katame", "Progressive");
     expect(HAS_KOI).not.toBeTruthy();
-});
-
+});*/
+/*
 test("User has draggoned koi", async() => {
-    const HAS_KOI = await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "akaouka", "beta");
+    const HAS_KOI = 
+        await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "akaouka", "beta", "Collector");
     expect(HAS_KOI).toBeTruthy();
 })
 
 test("User uses capital K to mark ownership.", async() => {
-    const HAS_KOI = await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "akadai", "beta");
+    const HAS_KOI = 
+        await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "akadai", "beta", "Collector");
     expect(HAS_KOI).toBeTruthy();
 })
 
 test("User uses capital D to mark dragon.", async() => {
-    const HAS_KOI = await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "madai", "beta");
+    const HAS_KOI = 
+        await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "madai", "beta", "Collector");
     expect(HAS_KOI).toBeTruthy();
 });
 
 test("Color is case insensitve", async() => {
-    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "MudAI", "mukei"))
+    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "MudAI", "mukei", "Collector"))
         .toBeTruthy();
-    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "MUkurO", "mukei"))
+    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "MUkurO", "mukei", "Collector"))
         not.toBeTruthy();
 });
 
 test("Pattern is case insensitve", async() => {
-    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "mudai", "MukEi"))
+    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "mudai", "MukEi", "Collector"))
         .toBeTruthy();
-    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "mukuro", "MukEi"))
+    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "mukuro", "MukEi", "Collector"))
         .not.toBeTruthy();
 });
 
 test("Accents are ignored.", async() => {
-    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "chakoji", "mukei"))
+    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "chakoji", "mukei", "Collector"))
         .toBeTruthy();
-    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "makoji", "mukei"))
+    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "makoji", "mukei", "Collector"))
         not.toBeTruthy();
 });
 
 test("User with collector koi starting with letter n.", async() => {
-    const HAS_KOI = await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "mamido", "naisu");
+    const HAS_KOI = 
+        await UserSpreadsheet.hasKoi(SPREADSHEET_ID, "mamido", "naisu", "Collector");
     expect(HAS_KOI).toBeTruthy();
 });
 
@@ -93,9 +105,9 @@ test(
     "dash in the spreadsheet.", 
     async() => 
 {
-    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "ryomosu", "rakki"))
+    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "ryomosu", "rakki", "Collector"))
         .toBeTruthy();
-    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "mamosu", "rakki"))
+    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "mamosu", "rakki", "Collector"))
         .toBeTruthy();
 });
 
@@ -104,20 +116,22 @@ test(
     "dash in the spreadsheet.", 
     async() => 
 {
-    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "gushiro", "rakki"))
+    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "gushiro", "rakki", "Collector"))
         .toBeTruthy();
-    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "guukon", "rakki"))
+    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "guukon", "rakki", "Collector"))
         .toBeTruthy();
 });
 
 test("Error getting whether the user has a koi of a non existant pattern.", async () => {
-    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "gushiro", "fakepattern"))
-        .rejects.toThrow(ErrorMessages.USER_SPREADSHEET.PATTERN_DOES_NOT_EXIST);
+    await expect(
+        UserSpreadsheet.hasKoi(SPREADSHEET_ID, "gushiro", "fakepattern", "Collector")
+    ).rejects.toThrow(ErrorMessages.USER_SPREADSHEET.PATTERN_DOES_NOT_EXIST);
 });
 
 test("Error getting whether the user has a koi of a non existant color.", async () => {
-    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "fakecolor", "rozu"))
-        .rejects.toThrow(ErrorMessages.USER_SPREADSHEET.COLOR_DOES_NOT_EXIST);
+    await expect(
+        UserSpreadsheet.hasKoi(SPREADSHEET_ID, "fakecolor", "rozu", "Collector")
+    ).rejects.toThrow(ErrorMessages.USER_SPREADSHEET.COLOR_DOES_NOT_EXIST);
 });
 
 test(
@@ -125,14 +139,16 @@ test(
     "but has a valid base and highlight color.", 
     async () => 
 {
-    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "nefakeshiro", "rozu"))
-        .rejects.toThrow(ErrorMessages.USER_SPREADSHEET.COLOR_DOES_NOT_EXIST);
+    await expect(
+        UserSpreadsheet.hasKoi(SPREADSHEET_ID, "nefakeshiro", "rozu", "Collector")
+    ).rejects.toThrow(ErrorMessages.USER_SPREADSHEET.COLOR_DOES_NOT_EXIST);
 });
 
 test(
     "Error getting whether the user has a koi when the spreadsheet ID is invalid.",
     async() => 
 {
-    await expect(UserSpreadsheet.hasKoi(SPREADSHEET_ID, "nefakeshiro", "rozu"))
-        .rejects.toThrow(ErrorMessages.USER_SPREADSHEET.CANNOT_GET_SPREADSHEET);
-});
+    await expect(
+        UserSpreadsheet.hasKoi(SPREADSHEET_ID, "nefakeshiro", "rozu", "Collector")
+    ).rejects.toThrow(ErrorMessages.USER_SPREADSHEET.CANNOT_GET_SPREADSHEET);
+});*/
