4a43d73c2cfba7d2ad24f770a2fa6d6d331a0932
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2022-02-01 10:49:40 -0500

    improved testing for community spreadsheet

--- src/google/spreadsheet.ts
+++ src/google/spreadsheet.ts
@@ -83,8 +83,7 @@ const Spreadsheet = {
                 error
             );
         }
-        console.log(values);
-
+        
         // when all the values are empty text,
         // google for some reason returns null or undefined
         // change that to be an empty list
--- tests/google/communitySpreadsheet.test.js
+++ tests/google/communitySpreadsheet.test.js
@@ -4,48 +4,57 @@ const { CommunitySpreadsheet } =require("../../src/google/communitySpreadsheet")
 // =====OVERVIEW=====
 // ==================
 
-test("Overview includes collectors.", async () => {
-    let overview = await CommunitySpreadsheet.getOverview();
-    expectOverviewHasType("Collector");
-});
-
-test("Overview includes progressives.", async () => {
-    let overview = await CommunitySpreadsheet.getOverview();
-    expectOverviewHasType(overview, "Progressive");
-});
-
-function expectOverviewHasType(overview, type)
+testOverviewHasType("Collector");
+testOverviewHasType("Progressive");
+function testOverviewHasType(type)
 {
-    let hasType = false;
-    for (let entry of overview)
-    {
-        if (entry.type == type)
+    test(`Overview includes ${type.toLowerCase()}s.`, async() => {
+        const OVERVIEW = await CommunitySpreadsheet.getOverview();
+        let hasType = false;
+        for (const ENTRY of OVERVIEW)
         {
-            hasType = true;
+            if (ENTRY.type == type)
+            {
+                hasType = true;
+                break;
+            }
+        }
+        expect(hasType).toBeTruthy();
+    })
+}
+
+test("Overview has no types other than progressive and collector.", async() => {
+    const OVERVIEW = await CommunitySpreadsheet.getOverview();
+    let hasWrongType = false;
+    for (const ENTRY of OVERVIEW)
+    {
+        if (ENTRY.type != "Progressive" && ENTRY.type != "Collector")
+        {
+            hasWrongType = true;
             break;
         }
     }
-    expect(hasType).toBeTruthy();
-}
+    expect(hasWrongType).not.toBeTruthy();
+})
 
 test("There are 30 progressives.", async () => {
-    let overview = await CommunitySpreadsheet.getOverview();
-    let count = getOverviewCountOfType(overview, "Progressive");
-    expect(count).toBe(30);
+    const OVERVIEW = await CommunitySpreadsheet.getOverview();
+    const COUNT = getOverviewCountOfType(OVERVIEW, "Progressive");
+    expect(COUNT).toBe(30);
 });
 
 test("There are at least 208 collectors.", async () => {
-    let overview = await CommunitySpreadsheet.getOverview();
-    let count = getOverviewCountOfType(overview, "Collector");
-    expect(count).toBeLessThanOrEqual(208);
+    let OVERVIEW = await CommunitySpreadsheet.getOverview();
+    let COUNT = getOverviewCountOfType(OVERVIEW, "Collector");
+    expect(COUNT).toBeLessThanOrEqual(208);
 });
 
 function getOverviewCountOfType(overview, type)
 {
     let count = 0;
-    for (let entry of overview)
+    for (const ENTRY of overview)
     {
-        if (entry.type == type)
+        if (ENTRY.type == type)
         {
             count++;
         }
@@ -62,13 +71,13 @@ function testOverviewHasPattern(name, type, hatchTime)
         `${name} is a ${type.toLowerCase()} with a hatch time of ${hatchTime} hours.`, 
         async () => 
     {
-        let overview = await CommunitySpreadsheet.getOverview();
+        const OVERVIEW = await CommunitySpreadsheet.getOverview();
         let found = false;
-        for (let entry of overview)
+        for (const ENTRY of OVERVIEW)
         {
-            if (entry.name == name && 
-                entry.type == type && 
-                entry.hatchTime == hatchTime
+            if (ENTRY.name == name && 
+                ENTRY.type == type && 
+                ENTRY.hatchTime == hatchTime
             ) 
             {
                 found = true;
@@ -78,3 +87,63 @@ function testOverviewHasPattern(name, type, hatchTime)
         expect(found).toBeTruthy();
     });
 }
+
+// ==============
+// =====KOIS=====
+// ==============
+
+testKoisIncludeRarity("Common");
+testKoisIncludeRarity("Rare");
+function testKoisIncludeRarity(rarity)
+{
+    test(`There are ${rarity.toLowerCase()} koi.`, async() => {
+        const KOIS = await CommunitySpreadsheet.getKois();
+        let hasRarity = false;
+        for (const KOI of KOIS)
+        {
+            if (KOI.rarity == rarity)
+            {
+                hasRarity = true;
+                break;
+            }
+        }
+        expect(hasRarity).toBeTruthy();
+    });
+}
+
+test("Kois are only common or rare.", async() => {
+    const KOIS = await CommunitySpreadsheet.getKois();
+    let hasWrongRarity = false;
+    for (const KOI of KOIS)
+    {
+        if (KOI.rarity != "Common" && KOI.rarity != "Rare")
+        {
+            hasWrongRarity = true;
+            break;
+        }
+    }
+    expect(hasWrongRarity).not.toBeTruthy();
+});
+
+test("There are at least 7,616 koi.", async () => {
+    const KOIS = await CommunitySpreadsheet.getKois();
+    expect(KOIS.length).toBeGreaterThanOrEqual(7616);
+});
+
+test("There are the same number of common as rare koi.", async () => {
+    const KOIS = await CommunitySpreadsheet.getKois();
+    let commonCount = 0;
+    let rareCount = 0;
+    for (const KOI of KOIS)
+    {
+        if (KOI.rarity == "Common")
+        {
+            commonCount++;
+        }
+        else if (KOI.rarity == "Rare")
+        {
+            rareCount++;
+        }
+    }
+    expect(commonCount).toBe(rareCount);
+});
