9efdfbe7a83e4f48403dd263eea249546d109b79
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2022-02-04 13:38:23 -0500

    added pattern property tests back

--- tests/database/models/pattern.test.js
+++ tests/database/models/pattern.test.js
@@ -2,22 +2,6 @@ const { initModel, Pattern } = require("../../../src/database/models/pattern");
 const { initSequelize, dropAllTables, getColumns } = require("../../_setup/database");
 const BaseModelTester = require("./_baseModelTester");
 
-/*
-let sequelize;
-
-beforeEach(async() => {
-    await dropAllTables();
-    
-    // function to test
-    sequelize = initSequelize();
-    await initModel(sequelize);
-    await sequelize.sync();
-});
-
-afterEach(async() => sequelize.close());
-
-afterAll(async() => await dropAllTables());*/
-
 BaseModelTester.runColumnTests(
     initModel,
     "patterns",
@@ -26,57 +10,71 @@ BaseModelTester.runColumnTests(
     [ "hatch_time" ]
 );
 
+describe("Model property tests.", () => {
+    let sequelize;
 
-// =========================
-// =====PROPERTY EXISTS=====
-// =========================
-/*
-describe("Model properties.", () => {
-    const PATTERN_TO_SAVE = {
-        name: "somename", 
-        type: "sometype",
-        hatchTime: 99
-    };
-    let savedPattern;
     beforeEach(async() => {
-        await Pattern.create(PATTERN_TO_SAVE);
-        savedPattern = await Pattern.findOne();
+        await dropAllTables();
+        
+        // function to test
+        sequelize = initSequelize();
+        await initModel(sequelize);
+        await sequelize.sync();
     });
-    
-    testPropertyExists("type");
-    testPropertyExists("name");
-    testPropertyExists("hatchTime");
-    function testPropertyExists(propertyName)
-    {
-        test(`Property ${propertyName} exists.`, () => {
-            expect(savedPattern[propertyName]).toBe(PATTERN_TO_SAVE[propertyName]);
+
+    afterEach(async() => await sequelize.close());
+
+    afterAll(async() => await dropAllTables());
+
+    // =========================
+    // =====PROPERTY EXISTS=====
+    // =========================
+
+    describe("Property exists.", () => {
+        const PATTERN_TO_SAVE = {
+            name: "somename", 
+            type: "sometype",
+            hatchTime: 99
+        };
+        let savedPattern;
+        beforeEach(async() => {
+            await Pattern.create(PATTERN_TO_SAVE);
+            savedPattern = await Pattern.findOne();
         });
-    }
+        testPropertyExists("discordId");
+        testPropertyExists("name");
+        testPropertyExists("spreadsheetId");
+        function testPropertyExists(propertyName)
+        {
+            test(`Property ${propertyName} exists.`, () => {
+                expect(savedPattern[propertyName]).toBe(PATTERN_TO_SAVE[propertyName]);
+            });
+        }
+    });
+
+    // =============================
+    // =====PROPERTY ATTRIBUTES=====
+    // =============================
+
+    test("Property name is required.", async() => {
+        await expect(
+            Pattern.create({type: "some type", hatch_time: 99 })
+        ).rejects.toThrow();
+    });
+
+    test("Property type is required.", async() => {
+        await expect(
+            Pattern.create({name: "some name", hatch_time: 99 })
+        ).rejects.toThrow();
+    });
+
+    test("Property hatch_time is optional.", async() => {
+        await Pattern.create({ name: "withNullTime", type: "some type" });
+        const SAVED_PATTERN = 
+            await Pattern.findOne({where: {name: "withNullTime"}});
+        expect(SAVED_PATTERN).toBeDefined();
+        expect(SAVED_PATTERN.name).toBe("withNullTime");
+        expect(SAVED_PATTERN.type).toBe("some type");
+        expect(SAVED_PATTERN.hatch_time).toBeUndefined();
+    });
 });
-
-
-// =============================
-// =====PROPERTY ATTRIBUTES=====
-// =============================
-
-test("Property name is required.", async() => {
-    await expect(
-        Pattern.create({type: "some type", hatch_time: 99 })
-    ).rejects.toThrow();
-});
-
-test("Property type is required.", async() => {
-    await expect(
-        Pattern.create({name: "some name", hatch_time: 99 })
-    ).rejects.toThrow();
-});
-
-test("Property hatch_time is optional.", async() => {
-    await Pattern.create({ name: "withNullTime", type: "some type" });
-    const SAVED_PATTERN = 
-        await Pattern.findOne({where: {name: "withNullTime"}});
-    expect(SAVED_PATTERN).toBeDefined();
-    expect(SAVED_PATTERN.name).toBe("withNullTime");
-    expect(SAVED_PATTERN.type).toBe("some type");
-    expect(SAVED_PATTERN.hatch_time).toBeUndefined();
-});*/
