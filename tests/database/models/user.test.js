da9797cab02f66a76086c5b2c00d4851308b9626
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2022-02-04 16:18:23 -0500

    refactor includes user

--- tests/database/models/user.test.js
+++ tests/database/models/user.test.js
@@ -1,78 +1,27 @@
-const { initModel, User } = require("../../../src/database/models/user");
-const { initSequelize, dropAllTables } = require("../../_setup/database");
 const BaseModelTester = require("./_baseModelTester");
+const { initModel, User } = require("../../../src/database/models/user");
 
-BaseModelTester.runColumnTests(
-    initModel,
-    "users",
-    [ "discord_id", "name", "spreadsheet_id" ],
-    "discord_id",
-    []
-);
+const USER = { discordId: "did", name: "somename", "spreadsheetId": "sid" };
 
-describe("Model properties.", () => {
-    let sequelize;
+BaseModelTester.runCommonTests({
 
-    beforeEach(async() => {
-        await dropAllTables();
-        
-        // function to test
-        sequelize = initSequelize();
+    init: async function(sequelize)
+    {
         await initModel(sequelize);
-        await sequelize.sync();
-    });
+    },
 
-    afterEach(async() => await sequelize.close());
+    tableName: "users",
+    columnNames: [ "discord_id", "name", "spreadsheet_id" ],
+    primaryKey: "discord_id",
 
-    afterAll(async() => await dropAllTables());
-
-    // =========================
-    // =====PROPERTY EXISTS=====
-    // =========================
-
-    describe("Property exists.", () => {
-        const USER_TO_SAVE = {
-            discordId: "somediscord", 
-            name: "somename", 
-            spreadsheetId: "somespreadsheet"
-        };
-        let savedUser;
-        beforeEach(async() => {    
-            await User.create(USER_TO_SAVE);
-            savedUser = await User.findOne();
-        });
-        testPropertyExists("discordId");
-        testPropertyExists("name");
-        testPropertyExists("spreadsheetId");
-        function testPropertyExists(propertyName)
-        {
-            test(`Property ${propertyName} exists.`, () => {
-                expect(savedUser[propertyName]).toBe(USER_TO_SAVE[propertyName]);
-            });
-        }
-    });
-
-    // =============================
-    // =====PROPERTY ATTRIBUTES=====
-    // =============================
-
-    test("Property discordId is required.", async() => {
-        await expect(
-            User.create({name: "some name", spreadsheetId: "some spreadsheet" })
-        ).rejects.toThrow();
-    });
-
-    test("Property name is required.", async() => {
-        await expect(
-            User.create({discordId: "some discord id", spreadsheetId: "some spreadsheet" })
-        ).rejects.toThrow();
-    });
-
-    test("Property spreadsheetId is required.", async() => {
-        await expect(
-            User.create({discordId: "some discordid", name: "some name" })
-        ).rejects.toThrow();
-    });
-    
-    
+    recordExample: USER,
+    beforePropertyExistsTests: async function() {
+        await User.create(USER);
+        const SAVED_USER = await User.findOne();
+        return SAVED_USER;
+    },
+    create: async function(user)
+    {
+        await User.create(user);
+    }
 });
