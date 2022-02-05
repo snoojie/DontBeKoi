35f70e184a58a967bc31599d1597701b311d98c4
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2022-02-05 10:06:08 -0500

    /who: Added comments. Also removed the comma between discord mentions.

--- src/commands/who.ts
+++ src/commands/who.ts
@@ -19,24 +19,30 @@ const WhoCommand: Command = {
         const COLOR: string = interaction.options.getString("color") || "";
         const PATTERN: string = interaction.options.getString("pattern") || "";
         
+        // get everyone who does not have this koi
         const USERS_MISSING_KOI: UsersMissingKoiResponse =
             await DataAccessLayer.getUsersMissingKoi(COLOR, PATTERN);
 
+        // start building the reply message
+        // to show information about the koi
+        // <rarity> <color> <pattern>
         const KOI_DESCRIPTOR: string = 
             `${USERS_MISSING_KOI.rarity.toLowerCase()} ${COLOR} ${PATTERN}`;
 
+        // if no one needs the koi, return that
         if (USERS_MISSING_KOI.discordIds.length == 0)
         {
             return `Nobody needs ${KOI_DESCRIPTOR}.`;
         }
 
+        // at least one person needs this koi
         let mentions: string[] = [];
         for (const DISCORD_ID of USERS_MISSING_KOI.discordIds)
         {
             mentions.push(`<@${DISCORD_ID}>`);
         }
 
-        return `Needing ${KOI_DESCRIPTOR}:\n${mentions.join(", ")}`;
+        return `Needing ${KOI_DESCRIPTOR}:\n${mentions.join(" ")}`;
     }
 
 };
