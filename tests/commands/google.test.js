c2f47f72bf76933a92be606e9bab8ccd263fc8c2
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2022-02-10 14:33:20 -0500

    improved google command tests

--- tests/commands/google.test.js
+++ tests/commands/google.test.js
@@ -1,12 +1,19 @@
 const GoogleCommand = require("../../src/commands/google").default;
 
-test("Name is google", () => {
+test("Name is google.", () => {
     expect(GoogleCommand.name).toBe("google");
 });
 
-test("Has spreadsheet option", () => {
-    let options = GoogleCommand.options;
-    expect(options).toBeDefined();
-    expect(options.length).toBe(1);
-    expect(options[0].name == "spreadsheet");
+test("Has a description.", () => {
+    expect(GoogleCommand.description).toBeDefined();
+    expect(GoogleCommand.description.length > 0);
+});
+
+test("Has exactly one option.", () => {
+    expect(GoogleCommand.options).toBeDefined();
+    expect(GoogleCommand.options.length).toBe(1);
+});
+
+test("Has spreadsheet option.", () => {
+    expect(GoogleCommand.options[0].name == "spreadsheet");
 });
