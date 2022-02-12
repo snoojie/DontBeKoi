078c425d34f9a149c2e5d778cb4ff9b3097a593a
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2022-02-12 09:25:33 -0500

    changed comment

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import Bot from "./dontBeKoiBot";
 
 Bot.start()
     .catch(_ => { 
-        /* nothing to do. Bot.start() already printed the error for us. */ 
+        // nothing to do. Bot.start() already printed the error for us. 
     });
