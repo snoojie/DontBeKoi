b8be48abf088848f3856ef2b4ebd0950125d90bd
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2022-02-14 18:36:07 -0500

    EnhancedError wasn't displaying error name.

--- src/util/enhancedError.ts
+++ src/util/enhancedError.ts
@@ -3,6 +3,7 @@ export default class EnhancedError extends Error
     constructor(message: string, error?: any)
     {
         super(message);
+        this.name = this.constructor.name;
         
         if (error)
         {
